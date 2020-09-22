4eb35e92079c554882e36be167dda5f801259447
Yurii Levchenko <36551537+FyuroTEX@users.noreply.github.com>
2020-09-23 00:59:54 +0300

    Add files via upload

---
+++ vp/precache-manifest.bab1ea1d2d7c7f2a4ef47e9c0dd6b8bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a21a7f65b184fe5d8df36d059eb8d3d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a63080e607685ceb46b",
+    "url": "./static/css/main.19a0a173.chunk.css"
+  },
+  {
+    "revision": "65dae685f89352cc8014",
+    "url": "./static/js/2.5f7db454.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.5f7db454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a63080e607685ceb46b",
+    "url": "./static/js/main.1fcccf00.chunk.js"
+  },
+  {
+    "revision": "89386e6f96304a84b6a4",
+    "url": "./static/js/runtime-main.70541182.js"
+  },
+  {
+    "revision": "1aeecd60c245f84d7db8bd634f0e870b",
+    "url": "./static/media/UA.1aeecd60.svg"
+  },
+  {
+    "revision": "aefba3fef673e14305b112b0e82805fe",
+    "url": "./static/media/USA.aefba3fe.svg"
+  },
+  {
+    "revision": "8a2f6d96fb7110bc9b5fefc0f05e848c",
+    "url": "./static/media/contact_us.8a2f6d96.jpg"
+  }
+]);
